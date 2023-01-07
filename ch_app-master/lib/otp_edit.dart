f686f84fd7a51e721158c8e6584db3b05a2242ed
nandini <nandinidadwal321@gmail.com>
2023-01-07 12:29:38 +0530

    changes

---
+++ ch_app-master/lib/otp_edit.dart
@@ -0,0 +1,22 @@
+/*import 'package:flutter/material.dart';
+
+class Otpedit extends StatelessWidget {
+  Otpedit({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: textOneCtrl,
+      maxLength: 1,
+      textAlign: TextAlign.center,
+      decoration: InputDecoration(
+          counterText: '',
+          border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(8)),
+          hintText: "*",
+          filled: true,
+          fillColor: Colors.grey),
+    );
+  }
+}*/
---
+++ ch_app-master/lib/signup.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+import 'otp.dart';
+
+class Signup extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => InitState();
+}
+
+class InitState extends State<Signup> {
+  @override
+  Widget build(BuildContext context) {
+    return initWidget();
+  }
+
+  Widget initWidget() {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 250,
+              decoration: BoxDecoration(
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(80)),
+                  gradient: LinearGradient(colors: [
+                    (new Color(0xFFC8479D)),
+                    (new Color(0xFFE8884C))
+                  ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50),
+                      child: Image.asset("images/pic.png"),
+                      height: 90,
+                      width: 90,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(right: 20, top: 20),
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Register Here",
+                        style: TextStyle(fontSize: 30, color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Colors.white)
+                ],
+              ),
+              alignment: Alignment.center,
+              child: TextField(
+                cursorColor: Color(0xffF5591F),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.person,
+                      color: Color(0xffF5591F),
+                    ),
+                    hintText: "Full Name",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Colors.white)
+                ],
+              ),
+              alignment: Alignment.center,
+              child: TextField(
+                cursorColor: Color(0xffF5591F),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.phone,
+                      color: Color(0xffF5591F),
+                    ),
+                    hintText: "Phone Number",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+              padding: EdgeInsets.only(left: 20, right: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.grey[200],
+                boxShadow: [
+                  BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 50,
+                      color: Colors.white)
+                ],
+              ),
+              alignment: Alignment.center,
+              child: TextField(
+                obscureText: true,
+                cursorColor: Color(0xffF5591F),
+                decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.vpn_key,
+                      color: Color(0xffF5591F),
+                    ),
+                    hintText: "Enter your Password",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none),
+              ),
+            ),
+            GestureDetector(
+              onTap: () => {
+                Navigator.pushReplacement(
+                    context, MaterialPageRoute(builder: (context) => Otp()))
+              },
+              child: Container(
+                margin: EdgeInsets.only(left: 20, right: 20, top: 70),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                alignment: Alignment.center,
+                height: 54,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    (new Color(0xFFC8479D)),
+                    (new Color(0xFFE8884C))
+                  ], begin: Alignment.centerLeft, end: Alignment.centerRight),
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Colors.white)
+                  ],
+                ),
+                child: Text(
+                  "REGISTER",
+                  style: TextStyle(),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Already Member?"),
+                  GestureDetector(
+                    onTap: () => {},
+                    child: Text(
+                      "Login Now",
+                      style: TextStyle(color: Colors.orangeAccent),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
